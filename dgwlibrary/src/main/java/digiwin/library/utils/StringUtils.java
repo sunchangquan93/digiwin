50a1431f4f941eaba8afa63d52ec3500b5bd1740
xiemeng99 <806398370@qq.com>
2017-04-03 11:48:16 +0800

    增加StringUtils方法

--- dgwlibrary/src/main/java/digiwin/library/utils/StringUtils.java
+++ dgwlibrary/src/main/java/digiwin/library/utils/StringUtils.java
@@ -4,43 +4,34 @@ import android.widget.EditText;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
 /**
  * 常用方法
  */
-public class StringUtils
-{
+public class StringUtils {
     /**
      * 计算字符串的中文长度
      *
      * @param s
      * @return
      */
-    public static int getChineseNum(String s)
-    {
+    public static int getChineseNum(String s) {
         int num = 0;
         char[] myChar = s.toCharArray();
-        for (int i = 0; i < myChar.length; i++)
-        {
-            if ((char)(byte)myChar[i] != myChar[i])
-            {
+        for (int i = 0; i < myChar.length; i++) {
+            if ((char) (byte) myChar[i] != myChar[i]) {
                 num++;
             }
         }
         return num;
     }
 
-    public static String objToString(Object obj)
-    {
-        if (obj != null && !String.valueOf(obj).equals("") && !String.valueOf(obj).equals("null"))
-        {
+    public static String objToString(Object obj) {
+        if (obj != null && !String.valueOf(obj).equals("") && !String.valueOf(obj).equals("null")) {
             return String.valueOf(obj);
-        }
-        else
-        {
+        } else {
             return "";
         }
     }
@@ -48,25 +39,20 @@ public class StringUtils
     /**
      * 转换字符串，如果字符串是NULL,或者“null”，或者"",统一返回""
      *
-     * @param str
-     *            需要转换的字符串
+     * @param str 需要转换的字符串
      * @return String 统一返回空字符串
      */
-    public static String toString(String str)
-    {
-        if (null != str && !str.equals("") && !str.equals("null"))
-        {
+    public static String toString(String str) {
+        if (null != str && !str.equals("") && !str.equals("null")) {
             return str;
-        }
-        else
-        {
+        } else {
             return "0";
         }
     }
 
     /**
      * 检查字符串是否是空白：<code>null</code>、空字符串<code>""</code>或只有空白字符。
-     *
+     * <p>
      * <pre>
      * StringUtil.isBlank(null)      = true
      * StringUtil.isBlank("")        = true
@@ -75,24 +61,18 @@ public class StringUtils
      * StringUtil.isBlank("  bob  ") = false
      * </pre>
      *
-     * @param str
-     *            要检查的字符串
-     *
+     * @param str 要检查的字符串
      * @return 如果为空白, 则返回<code>true</code>
      */
-    public static boolean isBlank(String str)
-    {
+    public static boolean isBlank(String str) {
         int length;
 
-        if ((str == null) || ((length = str.length()) == 0) || str.equals("null"))
-        {
+        if ((str == null) || ((length = str.length()) == 0) || str.equals("null")) {
             return true;
         }
 
-        for (int i = 0; i < length; i++)
-        {
-            if (!Character.isWhitespace(str.charAt(i)))
-            {
+        for (int i = 0; i < length; i++) {
+            if (!Character.isWhitespace(str.charAt(i))) {
                 return false;
             }
         }
@@ -103,12 +83,10 @@ public class StringUtils
     /**
      * 判断字符串是否为空
      *
-     * @param string
-     *            设置字符串
+     * @param string 设置字符串
      * @return boolean 返回是否为空
      */
-    public static boolean isEmpty(String string)
-    {
+    public static boolean isEmpty(String string) {
         return string == null || string.length() == 0;
     }
 
@@ -118,25 +96,21 @@ public class StringUtils
      * @param number
      * @return int
      */
-    public static int parseInt(String number)
-    {
+    public static int parseInt(String number) {
         int intNumber = 0;
-        try
-        {
+        try {
             intNumber = Integer.parseInt(number.trim());
-        }
-        catch (NumberFormatException err)
-        {
+        } catch (NumberFormatException err) {
             err.printStackTrace();
         }
 
         return intNumber;
     }
+
     /**
      * 设置editext只读属性
      */
-    public static void setEditTextReadOnly(EditText view)
-    {
+    public static void setEditTextReadOnly(EditText view) {
         view.setCursorVisible(false); // 设置输入框中的光标不可见
         view.setFocusable(false); // 无焦点
         view.setFocusableInTouchMode(false); // 触摸时也得不到焦点
@@ -146,15 +120,11 @@ public class StringUtils
     /**
      * 判断字符串是否是整数
      */
-    public static boolean isInteger(String value)
-    {
-        try
-        {
+    public static boolean isInteger(String value) {
+        try {
             Integer.parseInt(value);
             return true;
-        }
-        catch (NumberFormatException e)
-        {
+        } catch (NumberFormatException e) {
             return false;
         }
     }
@@ -166,8 +136,7 @@ public class StringUtils
      * @param d2
      * @return
      */
-    public static double sum(double d1, double d2)
-    {
+    public static double sum(double d1, double d2) {
         BigDecimal bd1 = new BigDecimal(Double.toString(d1));
         BigDecimal bd2 = new BigDecimal(Double.toString(d2));
         return bd1.add(bd2).doubleValue();
@@ -180,8 +149,7 @@ public class StringUtils
      * @param d2
      * @return
      */
-    public static double sub(double d1, double d2)
-    {
+    public static double sub(double d1, double d2) {
         BigDecimal bd1 = new BigDecimal(Double.toString(d1));
         BigDecimal bd2 = new BigDecimal(Double.toString(d2));
         return bd1.subtract(bd2).doubleValue();
@@ -194,8 +162,7 @@ public class StringUtils
      * @param d2
      * @return
      */
-    public static double mul(double d1, double d2)
-    {
+    public static double mul(double d1, double d2) {
         BigDecimal bd1 = new BigDecimal(Double.toString(d1));
         BigDecimal bd2 = new BigDecimal(Double.toString(d2));
         return bd1.multiply(bd2).doubleValue();
@@ -209,8 +176,7 @@ public class StringUtils
      * @param scale 四舍五入 小数点位数
      * @return
      */
-    public static double div(double d1, double d2, int scale)
-    {
+    public static double div(double d1, double d2, int scale) {
         // 当然在此之前，你要判断分母是否为0，
         // 为0你可以根据实际需求做相应的处理
 
@@ -221,21 +187,22 @@ public class StringUtils
 
     /**
      * 两个整数相除
+     *
      * @param a
      * @param b
      * @return
      */
-    public static int div(int a,int b){
+    public static int div(int a, int b) {
         int num = 0;
-            try{
-                if(a%b == 0){
-                    num = a/b;
-                }else{
-                    num = a/b + 1;
-                }
-            }catch(Exception e){
-                e.printStackTrace();
+        try {
+            if (a % b == 0) {
+                num = a / b;
+            } else {
+                num = a / b + 1;
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return num;
     }
 
@@ -244,18 +211,13 @@ public class StringUtils
      *
      * @return
      */
-    public static float string2Float(String string)
-    {
+    public static float string2Float(String string) {
         float a = 0f;
-        try
-        {
-            if (!StringUtils.isBlank(string))
-            {
+        try {
+            if (!StringUtils.isBlank(string)) {
                 a = Float.valueOf(string);
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             a = 0;
         }
         return a;
@@ -268,8 +230,7 @@ public class StringUtils
      * @param str2
      * @return
      */
-    public static float sum(String str1, String str2)
-    {
+    public static float sum(String str1, String str2) {
         BigDecimal bd1 = new BigDecimal(str1);
         BigDecimal bd2 = new BigDecimal(str2);
         return bd1.add(bd2).floatValue();
@@ -282,8 +243,7 @@ public class StringUtils
      * @param d2
      * @return
      */
-    public static float sub(String d1, String d2)
-    {
+    public static float sub(String d1, String d2) {
         BigDecimal bd1 = new BigDecimal(d1);
         BigDecimal bd2 = new BigDecimal(d2);
         return bd1.subtract(bd2).floatValue();
@@ -293,13 +253,11 @@ public class StringUtils
     /**
      * 根据，拆分
      */
-    public static List<String> split(String str)
-    {
+    public static List<String> split(String str) {
         List<String> list = new ArrayList<String>();
-        if (null != str)
-        {
+        if (null != str) {
             String[] split = str.split(",");
-            for(String o : split) {
+            for (String o : split) {
                 list.add(o);
             }
         }
@@ -307,10 +265,10 @@ public class StringUtils
     }
 
     /**
-     *  把后台传过来的\\n替换为\n
+     * 把后台传过来的\\n替换为\n
      */
-    public static String lineChange(String str){
-        if(null!=str){
+    public static String lineChange(String str) {
+        if (null != str) {
             String result = str.replace("\\n", "\n");
             return result;
         }
@@ -320,30 +278,24 @@ public class StringUtils
     /**
      * 整数去0
      */
-    public static String deleteZero(String str)
-    {
-        if (null != str)
-        {
-            String[] split = str.split("\\.");
-            if (split.length > 1 && StringUtils.string2Float(split[1]) == 0)
-            {
-                return split[0];
-            }
-            else
-            {
+    public static String deleteZero(String str) {
+        if (null != str) {
+            if(str.indexOf(".") > 0){
+                str = str.replaceAll("0+?$", "");//去掉多余的0
+                str = str.replaceAll("[.]$", "");//如最后一位是.则去掉
                 return str;
             }
         }
         return str;
     }
 
-
     /**
      * 取出其中小的数量
+     *
      * @return
      */
-    public static String getMinQty(String numb1,String numb2){
-        return  StringUtils.string2Float(numb1)<StringUtils.string2Float(numb2)?numb1:numb2;
+    public static String getMinQty(String numb1, String numb2) {
+        return StringUtils.string2Float(numb1) < StringUtils.string2Float(numb2) ? numb1 : numb2;
     }
 
 }
